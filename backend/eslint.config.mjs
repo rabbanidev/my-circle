efa67e62f0f71a45c5642ca7d90aacba91eb8256
rabbanidev <golamrabbani227417@gmail.com>
2025-09-28 18:47:38 +0600

    backend setup

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,59 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default [
+  {
+    ignores: ["node_modules", "dist"],
+  },
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: { process: "readonly" },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      // ---------- TypeScript ----------
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/ban-ts-comment": [
+        "warn",
+        { "ts-ignore": "allow-with-description" },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      // ---------- JavaScript/General ES Lint rules ----------
+      "no-unused-vars": "warn",
+      "prefer-const": "error",
+      "no-unused-expressions": "error",
+      "no-console": "warn",
+
+      // ---------- Prettier formate rules ----------
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+        },
+      ],
+    },
+  },
+];
